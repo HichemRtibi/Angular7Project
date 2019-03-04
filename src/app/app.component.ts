5ab9d75c9419db171101d382144623986ba6bd2e
hichem rtibi <hichem.rtibi@gmail.com>
2019-03-04 13:42:39 +0100

    creation de root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+
+@Component(
+  {
+    selector:'pm-root',
+    template:`
+      <h1>Acm product managment</h1>
+    `
+  }
+)
 export class AppComponent {
-  pageTitle = 'Acme product Management';
+
 }
